f7deb1ac9ae69a6aee7fa9f18e27d1995f1fa07a
Ben Greene <BenJGreene@gmail.com>
2014-03-23 20:06:06 +0000

    Initialised the project

---
+++ MemoryStorage/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  MemoryStorage
+//
+//  Created by Benjamin Greene on 23/03/2014.
+//  Copyright (c) 2014 Benjamin Greene. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
