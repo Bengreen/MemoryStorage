096fca0927b1d1c7801d0fc2ba5689ec26549c92
Ben Greene <BenJGreene@gmail.com>
2014-04-12 19:46:32 +0100

    renamed locks to define as global.

--- MemoryStorage/column.hh
+++ MemoryStorage/column.hh
@@ -1,10 +1,10 @@
-//
-//  column.h
-//  MemoryStorage
-//
-//  Created by Benjamin Greene on 30/03/2014.
-//  Copyright (c) 2014 Benjamin Greene. All rights reserved.
-//
+    //
+    //  column.h
+    //  MemoryStorage
+    //
+    //  Created by Benjamin Greene on 30/03/2014.
+    //  Copyright (c) 2014 Benjamin Greene. All rights reserved.
+    //
 
 #ifndef __MemoryStorage__column__
 #define __MemoryStorage__column__
@@ -15,16 +15,27 @@
 
 #include "objectLock.h"
 
+enum storageType {
+    globalLock,
+    atomicWrite
+};
+
+template<typename storage_t,
+storageType stoType=globalLock>
+class column {};
+
+
 template <typename storage_t>
-class column : objectLock {
+class column<storage_t, globalLock> : objectLock {
 private:
     int writeNum;
-    std::list<storage_t> myList;
+    typedef std::list<storage_t> listType_t;
+    listType_t myList;
 public:
     column(std::string name): objectLock(name), writeNum(0) {
         std::cout<<"Constructing column["<<name<<"]"<<std::endl;
     }
-
+    
     ~column() {
         std::cout<<"Destructing column["<<name<<"]"<<std::endl;
     }
@@ -42,12 +53,23 @@ public:
     
         // -------- Specialised for storage type
     void insert(storage_t& object) {
-            lock();
+        lock();
         myList.push_back(object);
-            release();
+        release();
     }
     unsigned long size() const {
         return myList.size();
+    }
+    
+    int sumAll() {
+        int sum(0);
+        lock();
+        for (typename listType_t::iterator myIter(myList.begin()); myIter!=myList.end(); ++myIter) {
+            sum+=*myIter;
+        }
+        release();
+        
+        return sum;
     }
     
         // ------- End of Specialised storage type
--- MemoryStorage/main.cpp
+++ MemoryStorage/main.cpp
@@ -14,14 +14,16 @@
 
 
 
-#define NUM_THREADS     5
-#define NUM_ENTRIES_PER_THREAD 100000
+int numThreadsWrite(5);
+int numThreadsRead(5);
+int entriesAddedPerThread(10000);
 
 struct batchObject_t {
     column<int>* colObj;
     int colNumber;
     int entriesPerThread;
     pthread_t threadId;
+    bool doRead;
 };
 
 
@@ -29,13 +31,30 @@ void* columnInsertBatch(void* columnObject) {
     
     batchObject_t* myObject((batchObject_t*)columnObject);
     
-    std::cout<<"Starting thread "<<myObject->colNumber<<std::endl;
+    std::cout<<"Starting WRITE thread "<<myObject->colNumber<<std::endl;
     
-    for(int i(0);i<NUM_ENTRIES_PER_THREAD;++i) {
+    for(int i(0);i<entriesAddedPerThread;++i) {
         myObject->colObj->insert(i);
     }
     
-    std::cout<<"Ending thread "<<myObject->colNumber<<std::endl;
+    std::cout<<"Ending WRITE thread "<<myObject->colNumber<<std::endl;
+    return 0;
+}
+
+void* columnRead(void* columnObject) {
+    
+    batchObject_t* myObject((batchObject_t*)columnObject);
+    
+    std::cout<<"Starting READ thread "<<myObject->colNumber<<std::endl;
+    int count(0);
+    
+    while (myObject->doRead) {
+        myObject->colObj->sumAll();
+        ++count;
+    }
+    
+    
+    std::cout<<"Ending READ thread "<<myObject->colNumber<<" with "<<count<<" reads"<<std::endl;
     return 0;
 }
 
@@ -47,23 +66,47 @@ int main(int argc, const char * argv[])
     
   
     {
-        batchObject_t objects[NUM_THREADS];
+        batchObject_t threadsWrite[numThreadsWrite],
+            threadsRead[numThreadsRead];
+        
+        
         
         column<int> colObj("PARALLEL HOLDER");
         
             //pthread_t columnThreads[NUM_THREADS];
         
-        for(int i(0);i<NUM_THREADS;++i) {
-            objects[i].entriesPerThread=NUM_ENTRIES_PER_THREAD;
-            objects[i].colObj=&colObj;
-            objects[i].colNumber=i;
+        for(int i(0);i<numThreadsWrite;++i) {
+            threadsWrite[i].entriesPerThread=entriesAddedPerThread;
+            threadsWrite[i].colObj=&colObj;
+            threadsWrite[i].colNumber=i;
             
-            pthread_create(&objects[i].threadId, NULL, columnInsertBatch, &objects[i]);
+            pthread_create(&threadsWrite[i].threadId, NULL, columnInsertBatch, &threadsWrite[i]);
         }
+        
+        for(int i(0);i<numThreadsRead;++i) {
+            threadsRead[i].doRead=true;
+            threadsRead[i].colObj=&colObj;
+            threadsRead[i].colNumber=i;
+            
+            pthread_create(&threadsRead[i].threadId, NULL, columnRead, &threadsRead[i]);
+        }
+        
+        
             //Threads running in parallel at this point
         
-        for(int i(0);i<NUM_THREADS;++i) {
-            pthread_join(objects[i].threadId, NULL);
+        //Collect stopped Writes
+        for(int i(0);i<numThreadsWrite;++i) {
+            pthread_join(threadsWrite[i].threadId, NULL);
+        }
+        
+            //When Writes are complete then stop reads
+        for(int i(0);i<numThreadsRead;++i) {
+            threadsRead[i].doRead=false;
+        }
+        
+            //Collect stopped Reads
+        for(int i(0);i<numThreadsRead;++i) {
+            pthread_join(threadsRead[i].threadId, NULL);
         }
         
         std::cout<<"Column contains :"<<colObj.size()<<std::endl;
